b993c1c4b8d091997f25467c160c158b3e60e52d
Siddharth Pawar <websitedev7849@gmail.com>
2021-09-09 19:45:00 +0530

    another comit

--- server/index.js
+++ server/index.js
@@ -4,10 +4,10 @@ const path = require("path");
 const PORT = process.env.PORT || 5300;
 const app = express();
 
-app.use(express.static(path.resolve(__dirname, '../client/build')));
+app.use(express.static(path.resolve(__dirname, '../frontend/build')));
 
 app.get("*", (req,res) => {
-    res.sendFile(path.resolve(__dirname, '../client/build', 'index.html'))
+    res.sendFile(path.resolve(__dirname, '../frontend/build', 'index.html'))
 })
 
 app.listen(PORT, () => {
